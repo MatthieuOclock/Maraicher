88d99de85335af3bc1289a1730ac066d7029aff5
Mathieu G <mathieu.gonon@gmail.com>
2024-01-19 15:42:58 +0100

    delete useless route

--- app/router.js
+++ app/router.js
@@ -4,6 +4,5 @@ import * as controllers from "./controllers.js";
 const router = express.Router();
 
 router.get('/about', controllers.about);
-router.get('/toto/titi', controllers.about);
 
 export default router;
